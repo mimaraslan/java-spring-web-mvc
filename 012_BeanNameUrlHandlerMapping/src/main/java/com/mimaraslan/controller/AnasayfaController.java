077eaf2fbc082405250d7ad2b42fafa279a8fbb2
mimaraslan <mimaraslan@gmail.com>
2015-05-11 17:58:36 +0300

    BeanNameUrlHandlerMapping
    BeanNameUrlHandlerMapping

---
+++ 012_BeanNameUrlHandlerMapping/src/main/java/com/mimaraslan/controller/AnasayfaController.java
@@ -0,0 +1,22 @@
+package com.mimaraslan.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class AnasayfaController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, 
+												 HttpServletResponse response) throws Exception {
+	
+		ModelAndView modelAndView = new ModelAndView("index");
+		
+		modelAndView.addObject("sayfa", "Anasayfa");
+		
+		return modelAndView;
+	}
+
+}
